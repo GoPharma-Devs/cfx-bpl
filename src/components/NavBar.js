d07c8115036d1ed16b487012fc48586e1382b6df
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-12-20 18:59:36 -0600

    ajute

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,10 @@
 import React from 'react'
+
 import { NavLink } from 'react-router-dom'
 import logoNav from "../../src/logo.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 function NavBar() {
   return (
     <nav>
@@ -11,6 +15,16 @@ function NavBar() {
             hemorrágicos</NavLink></li>
           <li> <NavLink className={({ isActive }) => isActive ? 'active-nav' : ''} to="/deficiencia_factor_x">Deficiencia
             Factor X</NavLink></li>
+          <li>
+            <a href="https://www.instagram.com/coagulacionfactorx/">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/coagulacionfactorx">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
