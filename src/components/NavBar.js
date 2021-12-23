d35186472dd96d2eb7fc6a9a9fc42a025dd488de
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-12-23 13:14:23 -0600

    ajuste nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { NavLink } from 'react-router-dom'
 import logoNav from "../../src/logo.svg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+
 function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <nav>
       <div className="contenedor-nav">
-        <NavLink to="/"> <img src={logoNav} alt="Coagulación Factor X" /></NavLink>
-        <ul className="menu">
-          <li> <NavLink className={({ isActive }) => isActive ? 'active-nav' : ''} to="/transtornos_hemorragicos">Trastornos
+        <NavLink to="/" onClick={closeMobileMenu}> <img src={logoNav} alt="Coagulación Factor X" /></NavLink>
+        <ul className={click ? "menu active-r" : "menu "}>
+          <li> <NavLink className={({ isActive }) => isActive ? 'active-nav' : ''} onClick={closeMobileMenu} to="/transtornos_hemorragicos">Trastornos
             hemorrágicos</NavLink></li>
-          <li> <NavLink className={({ isActive }) => isActive ? 'active-nav' : ''} to="/deficiencia_factor_x">Deficiencia
+          <li> <NavLink className={({ isActive }) => isActive ? 'active-nav' : ''} onClick={closeMobileMenu} to="/deficiencia_factor_x">Deficiencia
             Factor X</NavLink></li>
           <li>
             <a href="https://www.instagram.com/coagulacionfactorx/">
@@ -26,6 +31,9 @@ function NavBar() {
             </a>
           </li>
         </ul>
+        <div className={click ? "menu-icon fixed" : "menu-icon "} onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
       </div>
     </nav>
   )
